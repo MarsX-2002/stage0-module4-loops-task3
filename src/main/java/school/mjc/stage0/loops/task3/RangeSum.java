e38aa481545025fec1e27098aab043625d90cbf9
MarsX-2002 <mirjalol0331@gmail.com>
2023-04-02 11:37:27 +0500

    Update RangeSum.java

--- src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
@@ -1,11 +1,21 @@
 package school.mjc.stage0.loops.task3;
 
+import org.w3c.dom.css.CSSImportRule;
+
 public class RangeSum {
     public void printSumInclusive(int firstBoarder, int secondBoarder) {
         int sum = 0;
-        for (int i = firstBoarder; i < secondBoarder; i++){
-            sum+=i;
+        if (firstBoarder > secondBoarder) {
+            int temp = firstBoarder;
+            firstBoarder = secondBoarder;
+            secondBoarder = temp;
+        }
+        if (firstBoarder >= 0) {
+            for (int i = firstBoarder; i <= secondBoarder; i++) {
+                sum += i;
+            }
         }
         System.out.println(sum);
     }
 }
+
